f66879d13761460576a39510bb80abd7b5018dfe
Milanjiji <milanjiji0987654321@gmail.com>
2024-02-06 22:33:44 +0530

    removed notifee

--- components/Notification.js
+++ components/Notification.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import notifee, { TimestampTrigger, TriggerType, TimeUnit, EventType } from '@notifee/react-native';
 
-import { storage } from '../Storage'; // Import your storage library
 import messaging from '@react-native-firebase/messaging';
-import Details from '../Screens/Details';
 
 
 const NotificationComponent = () => {
@@ -25,22 +22,6 @@ const NotificationComponent = () => {
     const fetchTriggeredNotifications = async ()=>{
       const token  = await messaging().getToken()
       console.log(token);
-
-      const channelId = await notifee.createChannel({
-        id:'default',
-        name:'defaultChannel'
-      })
-
-      return notifee.onForegroundEvent(({type,detail})=>{
-        switch(type){
-          case EventType.DISMISSED:
-            console.log("user Dissmissed notifcation",detail.notification);
-            break;
-          case EventType.PRESS:
-            console.log("user Pressed Notifcation ",detail.notification);
-            break;
-        }
-      })
     }
     fetchTriggeredNotifications()
   },[])
